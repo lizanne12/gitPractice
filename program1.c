2a842b7d6386edd2f8a8d6c3778fea47030d5ca7
lizanne12 <23eee1014@nitgoa.ac.in>
2024-12-26 15:18:06 +0530

    Updated program1.c with a sum function

--- program1.c
+++ program1.c
@@ -1,6 +1,10 @@
-#include<stdio.h>
-int main()
-{
-	printf("Hello World\n");
-	return 0;
+#include <stdio.h>
+int sum(int a, int b) {
+    return a + b;
 }
+int main() {
+    printf("Hello World\n");
+    printf("Sum of 3 and 5 is: %d\n", sum(3, 5));
+    return 0;
+}
+
